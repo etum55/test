18483231cb3ba9f8531426b04e8dfc78abbabc28
etum55 <yodsapong.palm@gmail.com>
2023-06-27 22:10:20 +0700

    Exercise 8

--- Exercise 8_Yodsapong_S_.py
+++ Exercise 8_Yodsapong_S_.py
@@ -18,30 +18,34 @@ print("2.Blood Grenade : 65 Gold")
 print("3.Bottle : 675 Gold")
 print("4.Aghanim's Shard : 1,400 Gold")
 print("5.Blink Dagger : 2,250 Gold")
-
-userSelect = int(input("You want item number :"))
-if userSelect == 1:
-    x = int(input("How many : "))
-    price = 50
-    result = x * price
-    print("Total :",result,"Gold")
-elif userSelect == 2:
-    x = int(input("How many :"))
-    price = 65
-    result = x * price
-    print("Total :",result,"Gold")
-elif userSelect == 3:
-    x = int(input("How many :"))
-    price = 675
-    result = x * price
-    print("Total :",result,"Gold")
-elif userSelect == 4:
-    x = int(input("How many :"))
-    price = 1400
-    result = x * price
-    print("Total :",result,"Gold")
-elif userSelect == 5:
-    x = int(input("How many :"))
-    price = 2250
-    result = x * price
-    print("Total :",result,"Gold")
+userSelect = 0
+while userSelect != range(5):
+    userSelect = int(input("You want item number :"))
+    if userSelect == 1:
+        x = int(input("How many : "))
+        price = 50
+        result = x * price
+        print("Total :",result,"Gold")
+    elif userSelect == 2:
+        x = int(input("How many :"))
+        price = 65
+        result = x * price
+        print("Total :",result,"Gold")
+    elif userSelect == 3:
+        x = int(input("How many :"))
+        price = 675
+        result = x * price
+        print("Total :",result,"Gold")
+    elif userSelect == 4:
+        x = int(input("How many :"))
+        price = 1400
+        result = x * price
+        print("Total :",result,"Gold")
+    elif userSelect == 5:
+        x = int(input("How many :"))
+        price = 2250
+        result = x * price
+        print("Total :",result,"Gold")
+    else :
+        print("Try again !!!")
+print("--- THANK YOU ---")
