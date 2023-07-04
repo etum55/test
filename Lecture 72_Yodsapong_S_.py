d1e4623886f8c9b649f346668fc449f125aac037
etum55 <yodsapong.palm@gmail.com>
2023-07-04 22:39:22 +0700

    Lecture 72

---
+++ Lecture 72_Yodsapong_S_.py
@@ -0,0 +1,17 @@
+menuList = []
+def showBill():
+    totalPrice = 0
+    print("--- My Food ---")
+    for number in range(len(menuList)):
+        print(menuList[number])
+        totalPrice +=int(menuList[number][1])
+    print("Total : ",totalPrice)
+while True:
+    menuName = input("Please Enter Menu :")
+    if(menuName.lower() == ("exit")):
+        break
+    else:
+        menuPrice = input("Price :")
+        menuList.append([menuName,menuPrice])
+
+showBill()
